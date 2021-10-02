fe3726144c82b7634ce30f04825d9a4c74e955cc
xdth <info@dthlabs.com>
2021-10-02 17:24:55 -0400

    navbar

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -50,7 +50,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('user_index'));
+        return new RedirectResponse($this->urlGenerator->generate('log_index'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
