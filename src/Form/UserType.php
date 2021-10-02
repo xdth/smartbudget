18f2f65439de00c09f204a14586d6c9fd680339d
xdth <info@dthlabs.com>
2021-10-02 01:35:09 -0400

    UserType form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -13,12 +15,33 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
+            ->add('roles', 
+                  ChoiceType::class,
+                  [
+                    'required' => true,
+                    'multiple' => false,
+                    'expanded' => false,
+                    'choices' => [
+                      'User' => 'ROLE_USER',
+                      'Admin' => 'ROLE_ADMIN'
+                    ]
+                  ])
             ->add('password')
             ->add('email')
             ->add('telephone')
             ->add('details')
         ;
+
+        $builder->get('roles')
+                ->addModelTransformer(new CallbackTransformer(
+                  function($rolesArray) {
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                  },
+                  
+                  function($rolesString) {
+                    return [$rolesString];
+                  }
+                ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
