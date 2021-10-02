f895dd7a48e538cabc1fce4c69ab06d4c294a0e6
xdth <info@dthlabs.com>
2021-10-02 01:54:04 -0400

    UserController & password encoding

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,12 +9,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
- * @Route("/user")
+ * @Route("/users")
  */
 class UserController extends AbstractController
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+      $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @Route("/", name="user_index", methods={"GET"})
      */
@@ -30,22 +38,27 @@ class UserController extends AbstractController
      */
     public function new(Request $request): Response
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+      $user = new User();
+      $form = $this->createForm(UserType::class, $user);
+      $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+      if ($form->isSubmitted() && $form->isValid()) {
+          $entityManager = $this->getDoctrine()->getManager();
 
-            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
-        }
+          $plainpwd = $user->getPassword();
+          $encoded = $this->passwordEncoder->encodePassword($user, $plainpwd);
+          $user->setPassword($encoded);
 
-        return $this->renderForm('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+          $entityManager->persist($user);
+          $entityManager->flush();
+
+          return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+      }
+
+      return $this->renderForm('user/new.html.twig', [
+          'user' => $user,
+          'form' => $form,
+      ]);
     }
 
     /**
@@ -63,19 +76,26 @@ class UserController extends AbstractController
      */
     public function edit(Request $request, User $user): Response
     {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+      $form = $this->createForm(UserType::class, $user);
+      $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+      if ($form->isSubmitted() && $form->isValid()) {
+          $entityManager = $this->getDoctrine()->getManager();
 
-            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
-        }
+          $plainpwd = $user->getPassword();
+          $encoded = $this->passwordEncoder->encodePassword($user, $plainpwd);
+          $user->setPassword($encoded);
+          
+          $entityManager->persist($user);
+          $entityManager->flush();
 
-        return $this->renderForm('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+          return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+      }
+
+      return $this->renderForm('user/edit.html.twig', [
+          'user' => $user,
+          'form' => $form,
+      ]);
     }
 
     /**
