2ba4e137bbdf4cd74fd4d3498652b284219344c0
xdth <info@dthlabs.com>
2021-10-02 01:26:52 -0400

    crud Log

---
+++ src/Form/LogType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Log;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('operation')
+            ->add('value')
+            ->add('description')
+            ->add('details')
+            ->add('date')
+            ->add('category')
+            ->add('item')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Log::class,
+        ]);
+    }
+}
