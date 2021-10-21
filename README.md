55553374231577cffc71b52c192cb7cb6a3cf411
xdth <info@dthlabs.com>
2021-10-21 04:02:33 -0400

    readme

--- README.md
+++ README.md
@@ -1,14 +1,69 @@
-# smartbudget
+<h3 align="center">
+  smartbudget
+</h3>
 
-For the latest release, run:
+<p align="center">
+  <a href="https://dthlabs.com">
+    <img alt="Made by dthlabs" src="https://img.shields.io/badge/made%20by-dthlabs-%2304D361">
+  </a>
 
-sudo docker run --name smartbudget -it -p 8000:8000 dthlabs/smartbudget
-
-For the demo, run:
-
-sudo docker run --name smartbudget_demo -it -p 8000:8000 dthlabs/smartbudget_demo
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
+</p>
 
 
-Then access via:
+## What is the smartbudget app?
 
-http://localhost:8000
+Focus is an application that aims to help you better organize your day. It contains interesting features as
+well as astonishing background images and beautiful minimalist responsive design. It is built with vanilla Javascript and its whole code weights less than 25kb!
+
+smartbudget is an open-source application aiming to help you better organize your family finances. You can log your revenues and expenses, classify them by categories and compare to a previously defined budget, with the help of an amazing dashboard.
+
+
+## Features
+
+- User authentication: required in order to protect sensitive data;
+- User management: add, edit or remove users, set them as admin if needed;
+- Dashboard: visualize your data in a smart way;
+- Budget: your typical monthly expenses list;
+- Log: register your revenue and expenses;
+- Categories: add, edit or remove categories;
+- Items: add, edit or remove items (or sub-categories).
+
+
+## Docker images
+
+This app has two images, the latest app version and the demo, the latter containing dummy data for demonstration purposes.
+
+To run the images:
+
+- latest version: **`sudo docker run --name smartbudget -it -p 8000:8000 dthlabs/smartbudget`**
+
+- app demo: **`sudo docker run --name smartbudget_demo -it -p 8000:8000 dthlabs/smartbudget_demo`**
+
+Then visit http://localhost:8000 on your browser.
+
+
+## Screenshots
+
+ <table style="width:100%; border: none;">
+  <tr style="border: none;">
+    <td style="border: none;">
+      <img src="https://i.imgur.com/Kjg14nI.png" alt="screenshot">
+    </td>
+  </tr>
+  <tr style="border: none;">
+    <td style="border: none;">
+      <img src="https://i.imgur.com/YfdBMAA.png" alt="screenshot">
+    </td>
+  </tr>
+  <tr style="border: none;">
+    <td style="border: none;">
+      <img src="https://i.imgur.com/Qk4SaNQ.png" alt="screenshot">
+    </td>
+  </tr>
+  <tr style="border: none;">
+    <td style="border: none;">
+      <img src="https://i.imgur.com/VLgcJO7.png" alt="screenshot">
+    </td>
+  </tr>
+</table>
---
+++ migrations/Version20211021060237.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211021060237 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE budget (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER NOT NULL, item_id INTEGER NOT NULL, value DOUBLE PRECISION NOT NULL, description VARCHAR(255) DEFAULT NULL, details CLOB DEFAULT NULL)');
+        $this->addSql('CREATE INDEX IDX_73F2F77B12469DE2 ON budget (category_id)');
+        $this->addSql('CREATE INDEX IDX_73F2F77B126F525E ON budget (item_id)');
+        $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, details CLOB DEFAULT NULL)');
+        $this->addSql('CREATE TABLE item (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, details CLOB DEFAULT NULL)');
+        $this->addSql('CREATE INDEX IDX_1F1B251E12469DE2 ON item (category_id)');
+        $this->addSql('CREATE TABLE log (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER NOT NULL, item_id INTEGER NOT NULL, operation VARCHAR(255) NOT NULL, value DOUBLE PRECISION NOT NULL, description VARCHAR(255) DEFAULT NULL, details CLOB DEFAULT NULL, date DATE NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_8F3F68C512469DE2 ON log (category_id)');
+        $this->addSql('CREATE INDEX IDX_8F3F68C5126F525E ON log (item_id)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, telephone VARCHAR(255) DEFAULT NULL, details CLOB DEFAULT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE budget');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE item');
+        $this->addSql('DROP TABLE log');
+        $this->addSql('DROP TABLE user');
+    }
+}
